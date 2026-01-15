d00e87c6a505eecb8276dfb0d8718114aa172957
Arman-Grigoryan12356 <grigoryanarman527@gmail.com>
2026-01-16 03:23:38 +0400

    Add files via upload

---
+++ SXEMA 7/script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
